22ef1e3d2dd7845827e07c63161a99282e35f44a
German Tarabonda <57793086+kot239@users.noreply.github.com>
2022-05-21 00:47:55 +0300

    Add homework and submission

--- src/main/java/ru/java/sdhwproj/service/DefaultHomeworkService.java
+++ src/main/java/ru/java/sdhwproj/service/DefaultHomeworkService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import ru.java.sdhwproj.models.Homework;
 import ru.java.sdhwproj.models.Submission;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -12,22 +13,28 @@ import java.util.List;
 public class DefaultHomeworkService implements HomeworkService {
     @Override
     public Homework getHomeworkById(Long id) {
-        return null;
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        return new Homework(id, "Instasamka", "Pussy juicy", now, now);
     }
 
     @Override
     public Submission getSubmissionById(Long homeworkId, Long id) {
-        return null;
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        return new Submission(id, homeworkId, now, "Lipsi ha", 10, "Lipsi gimme money ha");
     }
 
     @Override
     public List<Homework> getAllHomeworks() {
-        return null;
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        Homework hw = new Homework(1L, "Instasamka", "Pussy juicy", now, now);
+        return List.of(hw);
     }
 
     @Override
     public List<Submission> getAllSubmissions(Long id) {
-        return null;
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        Submission submission = new Submission(1L, id, now, "Lipsi ha", 10, "Lipsi gimme money ha");
+        return List.of(submission);
     }
 
     @Override
